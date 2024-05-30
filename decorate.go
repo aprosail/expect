ef3a82cc7a7c5b059b97df37d673547b6c47aa03
James Aprosail <aprosail@outlook.com>
2024-05-30 08:03:58 +0800

    syntax sugar and color

---
+++ decorate.go
@@ -0,0 +1,5 @@
+package expect
+
+func dim(message string) string   { return "\x1b[2m" + message + "\x1b[0m" }
+func red(message string) string   { return "\x1b[31m" + message + "\x1b[0m" }
+func green(message string) string { return "\x1b[32m" + message + "\x1b[0m" }
--- expect.go
+++ expect.go
@@ -1,1 +1,32 @@
 package expect
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TestHandler struct {
+	tester *testing.T
+}
+
+func Init(t *testing.T) *TestHandler {
+	return &TestHandler{tester: t}
+}
+
+type ExpectHandler struct {
+	tester  *testing.T
+	handler any
+}
+
+func (t *TestHandler) Expect(actual any) *ExpectHandler {
+	return &ExpectHandler{tester: t.tester, handler: actual}
+}
+
+func (e *ExpectHandler) ToBe(expect any) {
+	if e.handler != expect {
+		e.tester.Error(
+			dim("Expect:"), green(fmt.Sprint(expect)),
+			dim("but get:"), red(fmt.Sprint(e.handler)),
+		)
+	}
+}
